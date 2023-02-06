50f682fab8c58d4cf9326bceeda454f4c3edb04b
Arvind PJ <arvindpunk@gmail.com>
2023-02-06 22:01:48 +0530

    feat: get word base on time

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/arvindpunk/word-proximity-service/internal/handlers"
 	"github.com/rs/zerolog/log"
 )
 
 func main() {
-	fmt.Println("Ligma")
 	r := handlers.NewRouter()
 	if err := r.Run(":5001"); err != nil {
 		log.Fatal().
--- internal/handlers/word.go
+++ internal/handlers/word.go
@@ -1,17 +1,50 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+)
 
 type ResponseTargetWord struct {
 	TargetWord string `json:"targetWord"`
 }
 
+var cache = make(map[int]string)
+
 func GetTargetWord() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		targetWord, err := getWordForTime(time.Now().UTC())
+		if err != nil {
+			log.Error().
+				Err(err).
+				Send()
+			WithError("ERR_DB_001", err.Error()).
+				Respond(c)
+			return
+		}
 		res := ResponseTargetWord{
-			TargetWord: "APPLE",
+			TargetWord: targetWord,
 		}
 
-		WithSuccess(res).Respond(c)
+		WithSuccess(res).
+			Respond(c)
 	}
+}
+
+// YYYY0000 +
+// 0000MM00 +
+// 000000DD
+// => year * 10000 + month * 100 + day = 20230206
+func getWordForTime(time time.Time) (string, error) {
+	time.Month()
+	year, month, day := time.Date()
+	dateInt := year*1000 + int(month) + day
+	targetWord, ok := cache[dateInt]
+	if !ok {
+		return "", errors.New("failed to find word in cache")
+	}
+	return targetWord, nil
 }
