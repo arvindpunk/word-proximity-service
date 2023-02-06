d1fdc8645a77b5b6b4081f2fc8d10ff5c4c17283
Arvind PJ <arvindpunk@gmail.com>
2023-02-06 10:01:18 +0530

    fix: allow cors all origins

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 
 	"github.com/arvindpunk/word-proximity-service/internal/handlers"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
 	fmt.Println("Ligma")
 	r := handlers.NewRouter()
-	r.Run()
+	if err := r.Run(":5001"); err != nil {
+		log.Fatal().
+			Err(err).
+			Msg("failed to start server")
+	}
 }
--- internal/handlers/router.go
+++ internal/handlers/router.go
@@ -1,11 +1,17 @@
 package handlers
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func NewRouter() *gin.Engine {
 	r := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	r.Use(
+		cors.New(config),
+	)
 	v1 := r.Group("/v1")
 	v1.GET("/get-target-word", GetTargetWord())
 	return r
